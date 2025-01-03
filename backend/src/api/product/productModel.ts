301fc2cdfc8f3ce640fee6332b961b3925cc66f0
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-01-03 15:59:38 +0200

    Add order and product models with validation schemas; implement repositories for data management

---
+++ backend/src/api/product/productModel.ts
@@ -0,0 +1,28 @@
+import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
+import { z } from 'zod';
+
+import { commonValidations } from '@/common/utils/commonValidation';
+
+extendZodWithOpenApi(z);
+
+export type Product = z.infer<typeof ProductSchema>;
+
+export const ProductSchema = z.object({
+  id: z.number(),
+
+  name: z.string(),
+  price: z.number(),
+  image: z.string(),
+  colors: z.array(z.string()),
+  company: z.string(),
+  description: z.string(),
+  category: z.string(),
+
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+// Input Validation for 'GET products/:id' endpoint
+export const GetProductSchema = z.object({
+  params: z.object({ id: commonValidations.id }),
+});
