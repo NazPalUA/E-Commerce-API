7da8ffd1094530fd0f42637c3d2c3cec1490125e
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-01-07 22:56:17 +0200

    Enhance authentication API with OpenAPI documentation and routing
    - Integrated OpenAPI registry for authentication routes including register, login, logout, and getCurrentUser.
    - Updated authRouter to register paths with request and response schemas for improved API documentation.
    - Modified OpenAPI document generator to include authRegistry, ensuring comprehensive API documentation across authentication endpoints.

--- backend/src/api/auth/authRouter.ts
+++ backend/src/api/auth/authRouter.ts
@@ -1,10 +1,63 @@
+import { createApiResponse } from '@/api-docs/openAPIResponseBuilders';
 import { authenticate } from '@/common/middleware/authenticate';
+import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
 import { Router } from 'express';
+import { z } from 'zod';
+import { User_DTO_Schema } from '../user/userModel';
 import { getCurrentUser, login, logout, register } from './authController';
+import { Login_Req_Schema, Register_Req_Schema } from './authModel';
 
+export const authRegistry = new OpenAPIRegistry();
 export const authRouter = Router();
 
+authRegistry.registerPath({
+  method: 'post',
+  path: '/register',
+  tags: ['Auth'],
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: Register_Req_Schema.shape.body,
+        },
+      },
+    },
+  },
+  responses: createApiResponse(User_DTO_Schema, 'Success'),
+});
+
 authRouter.post('/register', register);
+
+authRegistry.registerPath({
+  method: 'post',
+  path: '/login',
+  tags: ['Auth'],
+  request: {
+    body: {
+      content: {
+        'application/json': { schema: Login_Req_Schema.shape.body },
+      },
+    },
+  },
+  responses: createApiResponse(User_DTO_Schema, 'Success'),
+});
+
 authRouter.post('/login', login);
+
+authRegistry.registerPath({
+  method: 'post',
+  path: '/logout',
+  tags: ['Auth'],
+  responses: createApiResponse(z.null(), 'Success'),
+});
+
 authRouter.post('/logout', authenticate, logout);
+
+authRegistry.registerPath({
+  method: 'get',
+  path: '/me',
+  tags: ['Auth'],
+  responses: createApiResponse(User_DTO_Schema, 'Success'),
+});
+
 authRouter.get('/me', authenticate, getCurrentUser);
